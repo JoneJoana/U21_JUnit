8c8530694bf470ef19d3e71b59b874bef6797b27
David Marcos <david.13254@gmail.com>
2022-08-05 10:13:14 +0200

    testAreaCirculo y before()

--- Junit09_Geometria/src/test/java/JUnit/Junit09_Geometria/GeometriaTest.java
+++ Junit09_Geometria/src/test/java/JUnit/Junit09_Geometria/GeometriaTest.java
@@ -1,16 +1,32 @@
 package JUnit.Junit09_Geometria;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
 import JUnit.Junit09_Geometria.dto.Geometria;
 import org.junit.jupiter.api.Test;
 
 class GeometriaTest {
+	
+	Geometria geo;
+	
+	@BeforeEach
+	public void before() {
+		geo = new Geometria();
+	}
 
 	@Test
-	void testAreaCuadrado() {
-		Geometria geometria = new Geometria();
-		int resultado = geometria.areacuadrado(5);
+	public void testAreaCuadrado() {
+		int resultado = geo.areacuadrado(5);
 		int esperado = 25;
+		assertEquals(esperado, resultado);
+	}
+	
+	@Test
+	public void testAreaCirculo() {
+		double resultado = geo.areaCirculo(2);
+		double esperado = Math.PI*2*2;
 		assertEquals(esperado, resultado);
 	}
 	
