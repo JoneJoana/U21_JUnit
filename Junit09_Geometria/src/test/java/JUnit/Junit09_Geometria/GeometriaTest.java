815d6e06d38eb7645dedbbb473d9bc3197df7aa6
ionela vladasel <ionelavladasel2710@gmail.com>
2022-08-05 10:39:49 +0200

    test arearomboide, area trapecio

--- Junit09_Geometria/src/test/java/JUnit/Junit09_Geometria/GeometriaTest.java
+++ Junit09_Geometria/src/test/java/JUnit/Junit09_Geometria/GeometriaTest.java
@@ -8,9 +8,9 @@ import JUnit.Junit09_Geometria.dto.Geometria;
 import org.junit.jupiter.api.Test;
 
 class GeometriaTest {
-	
+
 	Geometria geo;
-	
+
 	@BeforeEach
 	public void before() {
 		geo = new Geometria();
@@ -22,16 +22,31 @@ class GeometriaTest {
 		int esperado = 25;
 		assertEquals(esperado, resultado);
 	}
-	
+
 	@Test
 	public void testAreaCirculo() {
 		int r = 2;
 		double delta = 0.01;
 		double resultado = geo.areaCirculo(r);
-		double esperado = Math.PI*Math.pow(r,2);
+		double esperado = Math.PI * Math.pow(r, 2);
 		assertEquals(esperado, resultado, delta);
 	}
-	
-	
+
+	@Test
+	public void arearomboide() {
+		int esperado = 6;
+		int resultado = geo.arearomboide(2, 3);
+		assertEquals(esperado, resultado);
+
+	}
+
+	@Test
+	public void areatrapecio() {
+		// ((B+b)/2)*h
+		int resultado = geo.areatrapecio(2, 2, 3);
+		int esperado = 6;
+		assertEquals(esperado, resultado);
+
+	}
 
 }
